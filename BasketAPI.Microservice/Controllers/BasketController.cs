a493d8107a290a6c832181a9aab8a9333ee3c5bb
Nelson Roza Junior <jr.1403@hotmail.com>
2021-05-30 12:27:09 -0300

    Add submodules

---
+++ BasketAPI.Microservice/Controllers/BasketController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BasketAPI.Microservice.Entities;
+using BasketAPI.Microservice.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace BasketAPI.Microservice.Controllers
+{
+    [Route("api/v1/[controller]")]  
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpGet("{userName}")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket)
+        {
+            if (basket is null)
+            {
+                return BadRequest("Invalid basket");
+            }
+
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+
+            return NoContent();
+        }
+    }
+}
